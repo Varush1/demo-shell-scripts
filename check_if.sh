a94863a5d4e014a0e70015805165d9e206db0166
Varush1 <ubuntu@ip-172-31-24-32.eu-west-1.compute.internal>
2025-02-19 05:56:19 +0000

    added check_if file to tracked

---
+++ check_if.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# User Defined Variables
+hero="Ram"
+villan="Shyam"
+
+
+echo "Who is the hero $hero"
+
+echo "who is the villan $villan"
+
+# Pre-defined variables
+
+echo "Current user is $USER"
+
+# user input
+read -p "What us the full name" fullname
+
+echo "Fullname of Ram is $fullname"
+
+# arguments
+
+
+# ./check_if Test1 Test2 Test3
+
+echo "Folder name: $0"
+
+echo "First user: $1"
+
+echo "Second user: $2"
+
+echo "Third user: $3"
+
+echo "the total number if users: $#"
+
+echo "Hence the users are $@"
+
+
+
+
